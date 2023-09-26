72e7b8b12029228b8cc06344d0cf4baae25a5b97
module_3_assignment <warashhasan@gmail.com>
2023-09-26 18:46:47 +0600

    2nd commit

--- task_4.php
+++ task_4.php
@@ -1,1 +1,31 @@
-
+<?php
+$studentGrades = [
+    'Rony' => [
+      'Math' => 90,
+      'English' => 85,
+      'Science' => 95
+    ],
+    'Roy' => [
+      'Math' => 80,
+      'English' => 90,
+      'Science' => 75
+    ],
+    'Rahul' => [
+      'Math' => 70,
+      'English' => 80,
+      'Science' => 90
+    ]
+  ];
+function calculateAverageGrade($studentGrades) {
+    foreach ($studentGrades as $studentName => $studentGrades) {
+      $totalGrade = 0;
+      foreach ($studentGrades as $subjectGrade) {
+        
+        $totalGrade += $subjectGrade;
+      }
+      $averageGrade = $totalGrade / count($studentGrades);
+      echo "$studentName's average grade is $averageGrade.\n";
+    }
+  }
+  calculateAverageGrade($studentGrades);
+?>
--- task_5.php
+++ task_5.php
@@ -1,1 +1,16 @@
+<?php
+function generatePassword($length) {
+    // Define the characters that can be used in the password
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+    $shuffledCharacters=str_shuffle($characters);
+    $subCharacter=substr($shuffledCharacters,0,$length);
+    return $subCharacter;
 
+    // Generate a random password of the specified length
+    // return substr(str_shuffle($characters), 0, $length);
+  }
+  $password = generatePassword(12);
+
+echo "The generated password is: $password.\n";
+
+?>
